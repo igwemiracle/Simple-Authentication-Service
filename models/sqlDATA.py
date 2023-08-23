f30226a6ac9bc4117cb9c4fcb902f08115eb2c19
igwemiracle <igwemiracle35@gmail.com>
2023-08-23 08:20:06 +0100

    complete project

--- models/sqlDATA.py
+++ models/sqlDATA.py
@@ -1,12 +1,13 @@
-from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy as sa
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = "signin"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column()
-    email: Mapped[str] = mapped_column(unique=True)
-    hash_password: Mapped[str] = mapped_column()
-    confirm_password: Mapped[str] = mapped_column()
+    id = sa.Column(sa.Integer, primary_key=True)
+    username = sa.Column(sa.String)
+    email = sa.Column(sa.String, unique=True, index=True)
+    hash_password = sa.Column(sa.String)
+    confirm_password = sa.Column(sa.String)
+    is_logged_in = sa.Column(sa.Boolean, default=False)
