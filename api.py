444e2849096444190469f14760f6ad6c9ec7127d
igwemiracle <igwemiracle35@gmail.com>
2023-08-23 08:18:36 +0100

    complete project

--- api.py
+++ api.py
@@ -5,7 +5,8 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(signup)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static",
+          StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port="8080")
