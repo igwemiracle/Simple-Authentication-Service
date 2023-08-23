7aad9240b7df7baf850d13c615f62245ca783219
igwemiracle <igwemiracle35@gmail.com>
2023-08-23 08:19:18 +0100

    complete project

--- routes/signup.py
+++ routes/signup.py
@@ -1,13 +1,11 @@
 from fastapi import Depends, APIRouter, Form, Request, status, HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
-from requests import Session
 from Database.connection import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from models.schemas import UserCreate
 from models.sqlDATA import User
 from fastapi.templating import Jinja2Templates
-from authenticate import cookie_auth
 from authenticate.hash_pwd import HashPassword
 
 hash_password = HashPassword()
@@ -27,14 +25,14 @@ async def renderHomePage(request: Request):
 
 
 @signup.get("/account")
-async def render_account_page(request: Request, username: str):
+async def render_account_page(request: Request, username: str = None, loggedin: bool = False):
     return templates.TemplateResponse(
         "account.html",
-        {"request": request, "username": username}
+        {"request": request, "username": username, "loggedin": loggedin}
     )
 
 
-@signup.get("/register", response_class=HTMLResponse)
+@signup.get("/account/register", response_class=HTMLResponse)
 async def renderRegisterPage(request: Request, db: AsyncSession = Depends(get_db)):
     results = await db.execute(select(User))
     users = results.scalars().all()
@@ -42,7 +40,7 @@ async def renderRegisterPage(request: Request, db: AsyncSession = Depends(get_db
     return templates.TemplateResponse("register.html", {"request": request, "users": users})
 
 
-@signup.post("/register")
+@signup.post("/account/register")
 async def registerUser(request: Request,
                        username: str = Form(...),
                        email: str = Form(...),
@@ -55,10 +53,40 @@ async def registerUser(request: Request,
             "register.html",
             {"request": request, "error_msg": error_msg}
         )
+
     dbUser = User(username=username, email=email,
                   hash_password=password, confirm_password=confirm_password)
     db.add(dbUser)
     await db.commit()
     await db.refresh(dbUser)
-    return RedirectResponse(url=f"/account?username={username}",
+    return RedirectResponse(url=f"/account?username={username}&loggedin=True",
                             status_code=status.HTTP_303_SEE_OTHER)
+
+
+@signup.get("/account/logout")
+async def logOutPage():
+    response = RedirectResponse(
+        url="/", status_code=status.HTTP_302_FOUND
+    )
+    return response
+# =====================================================================
+# @signup.post("/account/register")
+# async def registerUser(request: Request,
+#                        username: str = Form(...),
+#                        email: str = Form(...),
+#                        password: str = Form(...),
+#                        confirm_password: str = Form(...),
+#                        db: AsyncSession = Depends(get_db)):
+#     if password != confirm_password:
+#         error_msg = "Invalid password,please confirm password again."
+#         return templates.TemplateResponse(
+#             "register.html",
+#             {"request": request, "error_msg": error_msg}
+#         )
+#     dbUser = User(username=username, email=email,
+#                   hash_password=password, confirm_password=confirm_password)
+#     db.add(dbUser)
+#     await db.commit()
+#     await db.refresh(dbUser)
+#     return RedirectResponse(url=f"/account?username={username}",
+#                             status_code=status.HTTP_303_SEE_OTHER)
