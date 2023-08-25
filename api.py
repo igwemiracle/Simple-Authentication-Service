b9e618e1ea23c8fea59d3e9d8ef62b9807fe58c8
igwemiracle <igwemiracle35@gmail.com>
2023-08-25 01:33:03 +0100

    complete project

--- api.py
+++ api.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 from routes.signup import signup
+from routes.login import login
+from routes.passwordReset import resetPassword
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(signup)
+app.include_router(login)
+app.include_router(resetPassword)
 app.mount("/static",
           StaticFiles(directory="static"), name="static")
 
