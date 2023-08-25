184447d9096cced69668f3a0405f1307ecb90ba9
igwemiracle <igwemiracle35@gmail.com>
2023-08-25 01:32:06 +0100

    complete project

--- models/sqlDATA.py
+++ models/sqlDATA.py
@@ -11,3 +11,15 @@ class User(Base):
     hash_password = sa.Column(sa.String)
     confirm_password = sa.Column(sa.String)
     is_logged_in = sa.Column(sa.Boolean, default=False)
+
+
+class ResetCode(Base):
+    __tablename__ = "py_code"
+    id = sa.Column(sa.Integer, primary_key=True)
+    email = sa.Column(sa.String, unique=True, index=True)
+    reset_code = sa.Column(sa.String, unique=True, index=True)
+    status = sa.Column(sa.String(1))
+    expired_in = sa.Column(sa.DateTime)
+
+
+
